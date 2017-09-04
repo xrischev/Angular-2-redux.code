83b951e1b0dfb24abb2a57dcc4b7ae6fecbe24d1
xrischev <shevchenko93@abv.bg>
2017-09-04 19:06:51 +0300

    angular2-redux-code

---
+++ stats.component.ts
@@ -0,0 +1,28 @@
+import {Component,OnInit} from '@angular/core'
+import {StatsActions} from '../store/stats/stats.actions'
+import {StatsModel} from './stats.model'
+import {NgRedux} from 'ng2-redux'
+import {IAppState} from '../store'
+
+
+@Component({
+  selector:'stats',
+  templateUrl:'./stats.component.html'
+})
+
+export class StatsComponent implements OnInit{
+stats:StatsModel=new StatsModel()
+
+  constructor(
+    private statsActions:StatsActions,
+    private ngRedux:NgRedux<IAppState>
+  ){}
+
+ngOnInit(){
+this.statsActions.get();
+this.ngRedux
+  .select(state=>state.stats)
+  .subscribe(stats=>this.stats=stats)
+
+}
+}
